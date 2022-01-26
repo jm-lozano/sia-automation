7fa7b3b58f52faf8b07645a2dd117e889d044c8f
0xDreiker <0xDreiker@gmail.com>
2022-01-26 19:53:04 +0100

    storage min/max price limit

--- linux.py
+++ linux.py
@@ -16,6 +16,8 @@ usd_upload_price = config["usd_upload_price"]
 usd_download_price = config["usd_download_price"]
 usd_storage_price = config["usd_storage_price"]
 collateral_ratio = config["collateral_ratio"]
+storage_price_min = config["siacoin_storage_price_min"]
+storage_price_max = config["siacoin_storage_price_max"]
 # ----------------------- CONFIGURATION --------------------------
 
 # Returns the USD/SC price listed in coinmarketcap.com
@@ -35,22 +37,29 @@ def get_siacoin_price():
         response = session.get(url, params=parameters)
         data = json.loads(response.text)
         price = data["data"]["SC"]["quote"]["USD"]["price"];
+        print("CMC Siacoin price is", price, "USD")
         return price
     except (ConnectionError, Timeout, TooManyRedirects) as e:
         print(e)
 
+def clamp(num, min_value, max_value):
+    num = max(min(num, max_value), min_value)
+    return num
+
 siacoin_price = get_siacoin_price()
+print("############ PRICE SETTINGS ############")
 download_price = usd_download_price / siacoin_price
-print("> Download price updating...", download_price, "SC")
+print("Download price updating to...", download_price, "SC")
 
 upload_price = usd_upload_price / siacoin_price
-print("> Upload price updating...  ", upload_price, "SC")
+print("Upload price updating to...  ", upload_price, "SC")
 
 storage_price = usd_storage_price / siacoin_price
-print("> Storage price updating... ", storage_price, "SC")
+storage_price = clamp(storage_price, storage_price_min, storage_price_max)
+print("Storage price updating to... ", storage_price, "SC")
 
 collateral = storage_price * collateral_ratio
-print("> Collateral updating...    ", collateral, "SC")
+print("Collateral updating to...    ", collateral, "SC")
 
 if platform.system() == "Linux":
     os.system("siac host config mindownloadbandwidthprice " + str(download_price) + "SC")
